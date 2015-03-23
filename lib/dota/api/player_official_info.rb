cedb352a6d3a3b1dae1fbfc7e49eeb7473145949
Eugene Nakonechny <eugene.src@gmail.com>
2015-03-23 23:04:28 +0200

    Add PlayerOfficialInfo class

---
+++ lib/dota/api/player_official_info.rb
@@ -0,0 +1,33 @@
+module Dota
+  module API
+    class PlayerOfficialInfo < Entity
+      def account_id
+        raw["account_id"]
+      end
+
+      def name
+        raw["Name"]
+      end
+
+      def team_name
+        raw["TeamName"]
+      end
+
+      def team_tag
+        raw["TeamTag"]
+      end
+
+      def sponsor
+        raw["Sponsor"]
+      end
+
+      def fantasy_role
+        raw["FantasyRole"]
+      end
+
+      def full_name
+        [team_tag, name, sponsor].select { |a| !a.empty? }.join "."
+      end
+    end
+  end
+end
